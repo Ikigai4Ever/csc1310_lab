8c5b91005da0b1376a955c5559ed83f5fde43706
Ty <ty.h.ahrens@gmail.com>
2024-09-13 10:56:16 -0500

    skeleton code for driver.cpp

--- Smirking_Companions_PQ/Driver.cpp
+++ Smirking_Companions_PQ/Driver.cpp
@@ -4,3 +4,14 @@
  *  DESCRIPTION:
  *  PROGRAM: Driver.cpp
  */
+
+#include "Personalities.h"
+#include "Quiz_Answers.h"
+#include "Quiz_Taker_Info.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
