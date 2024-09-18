5c8f236b34c1a1335b6337f385a898ab61b5ff4a
Ty <ty.h.ahrens@gmail.com>
2024-09-18 15:46:39 -0500

    Created answers class and questionsptr pointer

--- Smirking_Companions_PQ/Quiz_Answers.cpp
+++ Smirking_Companions_PQ/Quiz_Answers.cpp
@@ -1,6 +1,14 @@
 /* 
- *  NAME:
- *  DATE:
- *  DESCRIPTION:
-*  PROGRAM: Quiz_Answers.cpp
+ *  NAME: Ty Ahrens
+ *  DATE: 9/18/24
+ *  DESCRIPTION: Save quiz answers to questions array, 
+ *  PROGRAM: Quiz_Answers.cpp
  */
+
+class Answers{
+    private:
+        int questions[10];
+
+    public:
+        int *questionsptr = questions;
+};
