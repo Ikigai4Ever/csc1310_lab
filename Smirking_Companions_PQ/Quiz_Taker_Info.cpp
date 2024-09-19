c43ddc7a4391f727cd9b1e01094b0a6965e76888
eaunderwoo42 <eaunderwoo42@tntech.edu>
2024-09-19 15:54:15 -0500

    skeleton code for Quiz_Taker_Info.cpp

--- Smirking_Companions_PQ/Quiz_Taker_Info.cpp
+++ Smirking_Companions_PQ/Quiz_Taker_Info.cpp
@@ -1,6 +1,22 @@
 /* 
- *  NAME:
- *  DATE:
- *  DESCRIPTION:
+ *  NAME: Zander Underwood
+ *  DATE:  9/18/24
+ *  DESCRIPTION:    Store the people who take the test's information (first and last name, email, phone number) along with what they scored on the personality.
  *  PROGRAM: Quiz_Taker_Info.cpp
  */
+
+#include "Quiz_Taker_Info.h"
+
+//class decleration for quiz taker's info
+
+class Info
+{
+    public:
+        string firstName;
+        string lastName;
+        string email;
+        string phoneNum;
+
+    private:
+
+};
--- Smirking_Companions_PQ/Quiz_Taker_Info.h
+++ Smirking_Companions_PQ/Quiz_Taker_Info.h
@@ -1,6 +1,10 @@
 /* 
- *  NAME:
- *  DATE:
- *  DESCRIPTION:
+ *  NAME: Zander Underwood
+ *  DATE: 9/18/24
+ *  DESCRIPTION: Header File for the Quiz_Taker_Info.cpp file
  *  PROGRAM: Quiz_Taker_Info.cpp
  */
+
+#include <iostream>
+#include <string>
+using namespace std;
