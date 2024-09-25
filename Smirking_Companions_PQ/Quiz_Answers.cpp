f5fcb9ab26a761501aa2d372794c35ad443d9512
Ty <ty.h.ahrens@gmail.com>
2024-09-25 15:07:38 -0500

    Created 2 functions to get answers from the answers.txt file and to store the answers to the personality quiz to the answers.txt file

--- Smirking_Companions_PQ/Quiz_Answers.cpp
+++ Smirking_Companions_PQ/Quiz_Answers.cpp
@@ -1,6 +1,6 @@
 /* 
  *  NAME: Ty Ahrens
- *  DATE: 9/18/24
+ *  DATE: 9/25/24
  *  DESCRIPTION: Save quiz answers to questions array, 
  *  PROGRAM: Quiz_Answers.cpp
  */
@@ -11,4 +11,26 @@ class Answers{
 
     public:
         int *questionsptr = questions;
+        void Answers::getAnswers();
+        void Answers::storeAnswers();
 };
+
+/*
+ *      FUNCTION: getAnswers()
+ *      DESCRIPTION: WIP
+ */
+void Answers::getAnswers(){
+
+}
+
+/*
+ *      FUNCTION: storeAnswers()
+ *      DESCRIPTION: Store all of the answers that the user puts down 
+ *                   in the personality quiz in the answers.txt file
+ */
+
+void Answers::storeAnswers(){
+    for (int i = 0; i < 10; i++){
+
+    }
+}
