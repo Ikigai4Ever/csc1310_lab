b7926043734b86401959717fa77ab2fafb75cf86
bowmanevan <cebowman42@tntech.edu>
2024-09-25 15:34:59 -0500

    Revert "added skeleton class"
    This reverts commit 636c7a6e6efd4eeb3e02bcaa471026361eebaff2.

--- Smirking_Companions_PQ/Personalities.cpp
+++ Smirking_Companions_PQ/Personalities.cpp
@@ -1,24 +1,6 @@
 /* 
  *  NAME:
  *  DATE:
- *  DESCRIPTION: Store personality in txt file,  
+ *  DESCRIPTION: send personalities to quiz_taker_info 
  *  PROGRAM: Personalities.cpp
- * get personlaitys
- * total personalits
- * store personlaites
  */
-
-    #include <iostream>
-
-    using namespace std;
-
-    class P_Percent
-    {
-        private:
-        
-
-
-        public:
-
-
-    }
