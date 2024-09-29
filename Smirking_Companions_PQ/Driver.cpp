c99b234c2eecc1b964af5dafe60466a006fe3e94
bowmanevan <cebowman42@tntech.edu>
2024-09-28 19:25:49 -0500

    Finished all questions

--- Smirking_Companions_PQ/Driver.cpp
+++ Smirking_Companions_PQ/Driver.cpp
@@ -64,6 +64,7 @@ void personality_quiz(int* q_array,int SIZE, string name)
     cout << "5) Consult my board of shady and corrupt advisors and then \"get rid\" of the compitetion." << endl;
     cout << "Answer: ";
     //cin  >> q_array[0];
+
     cout << "QUESTION 2" << endl;
     cout << name << ",It is a tight race in the presidential vote. The two Candiates are President Jimble and Mr. Frog." << endl;
     cout << "Who are you going to vote for, " << name << "?" << endl;
@@ -74,9 +75,9 @@ void personality_quiz(int* q_array,int SIZE, string name)
     cout << "5) Why of course I voted for President Jimble! I got a 10,000 bet he will win! If he doesn't win there will be repercussions." << endl;
     cout << "Answer: ";
     //cin  >> q_array[1];
+
     cout << "QUESTION 3" << endl;
     cout << name << "If you could describe yourself in one word what would it be?" << endl;
-    
     cout << "1) ielinutle." << endl;
     cout << "2) Innocent." << endl;
     cout << "3) Chill." << endl;
@@ -87,7 +88,6 @@ void personality_quiz(int* q_array,int SIZE, string name)
 
     cout << "QUESTION 4" << endl;
     cout << name << ", if you could describe your physcial appearance in an Adult Swim animated TV show created by Zach Hadel and Michael Cusack, what would it look like?" << endl;
-    
     cout << "1) A small green creature with a purple hat that speaks a language only understandable by characters in the show." << endl;
     cout << "2) A short purple humanoid with an exposed nerve ending." << endl;
     cout << "3) A rotund yellow humanoid with a comically large nose." << endl;
@@ -96,6 +96,65 @@ void personality_quiz(int* q_array,int SIZE, string name)
     cout << "Answer: ";
     //cin  >> q_array[3];
 
+    cout << "QUESTION 5" << endl;
+    cout << name << ", out of the choices below which food would you eat?" << endl; 
+    cout << "1) Coffee and french fries." << endl;
+    cout << "2) Worms." << endl;
+    cout << "3) The Century Egg from Salty's." << endl;
+    cout << "4) An Isometric cut of Cheddar Cheese." << endl;
+    cout << "5) A warm plate of Spaghetti." << endl;
+    cout << "Answer: ";
+    //cin  >> q_array[4];
+
+    cout << "QUESTION 6" << endl;
+    cout << name << ", out of the choices below which activity would you wanna do?" << endl; 
+    cout << "1) Replacing the famous star Mr Frog on the Mr Frog Show." << endl;
+    cout << "2) Fetching Firewood from across the road for a Halloween party." << endl;
+    cout << "3) Being accosted by James to make him smile." << endl;
+    cout << "4) Finding your missing paper clips in a long winded attempt by your landlord to get you to hang out with him." << endl;
+    cout << "5) Visit Spamtopia to buy a Magical Red Jewel for your son Tyler." << endl;
+    cout << "Answer: ";
+    //cin  >> q_array[5];
+
+    cout << "QUESTION 7" << endl;
+    cout << name << ", which Smiling Friend do you think you are?" << endl; 
+    cout << "1) Glep." << endl;
+    cout << "2) Pim." << endl;
+    cout << "3) Charlie." << endl;
+    cout << "4) Alan." << endl;
+    cout << "5) The Boss" << endl;
+    cout << "Answer: ";
+    //cin  >> q_array[6];
+
+    cout << "QUESTION 8" << endl;
+    cout << name << ", what would you do if a snowman you created came to life?" << endl; 
+    cout << "1) olhear esûdait hþinfagiv dtyaiwƶ aseset lewyæ." << endl;
+    cout << "2) Keep the Snowman alive and show him the meaning of life!" << endl;
+    cout << "3) Not my responsibility, I have to go help my uncle with something." << endl;
+    cout << "4) I dont really care about the snowman, I have to go to my Doctor Monsters appointment." << endl;
+    cout << "5) Destroy the snowman because of its newfound consciousness." << endl;
+    cout << "Answer: ";
+    //cin  >> q_array[7];
+
+    cout << "QUESTION 9" << endl;
+    cout << name << ", who killed Simon S. Salty?" << endl; 
+    cout << "1) Mr. Ketchup" << endl;
+    cout << "2) The Fun Twins." << endl;
+    cout << "3) Mrs. Mustard." << endl;
+    cout << "4) Salt and Pepper." << endl;
+    cout << "5) Crazy Cup." << endl;
+    cout << "Answer: ";
+    //cin  >> q_array[8];
+
+    cout << "QUESTION 10" << endl;
+    cout << name << ", what would dress up as for Halloween?" << endl; 
+    cout << "1) Matching costumes as Ronald and Nancy Reagan." << endl;
+    cout << "2) A Cowboy." << endl;
+    cout << "3) I'm not dressing up becasue I am scared of what will be considered offensive or what could become offensive later on." << endl;
+    cout << "4) Joaquin Phoenix's Joker." << endl;
+    cout << "5) Charlie Brown." << endl;
+    cout << "Answer: ";
+    //cin  >> q_array[9]; 
 }
 
 
