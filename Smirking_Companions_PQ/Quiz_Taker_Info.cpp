1b5943fce7f581781d6e21c300e36e3bf555e21c
eaunderwoo42 <eaunderwoo42@tntech.edu>
2024-10-01 10:15:32 -0500

    Added printing information to text file

--- Smirking_Companions_PQ/Quiz_Taker_Info.cpp
+++ Smirking_Companions_PQ/Quiz_Taker_Info.cpp
@@ -1,12 +1,13 @@
 /* 
  *  NAME: Zander Underwood
- *  DATE:  9/18/24
+ *  DATE:  9/27/24
  *  DESCRIPTION:    Store the people who take the test's information (first and last name, email, phone number) along with what they scored on the personality.
  *  PROGRAM: Quiz_Taker_Info.cpp
  */
 
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 #include "Quiz_Taker_Info.h"
@@ -26,9 +27,23 @@ class Info
 
 void Info::setinfo(string firstName,string lastName,string email,string phoneNum)
 {
+    fstream Infofile;
+
     this->firstName = firstName;
     this->lastName = lastName;
     this->email = email;
     this->phoneNum = phoneNum;
 
+    Infofile.open("Quiz_Taker_Info.txt");
+    if(Infofile.is_open())
+    {
+        Infofile <<this->firstName <<"#";
+        Infofile << this->lastName<<"#";
+        Infofile << this->email<<"#";
+        Infofile << this->phoneNum<<"#";
+    }
+    else
+    {
+        cout << "Couldn't find the text file." << endl;
+    }
 }
